5cfe754d800537ef95a1b24c7ec2bd60d1e5b033
guerrayuri <166180584+guerrayuri@users.noreply.github.com>
2025-05-01 12:25:22 -0300

    Update cinema.c

--- cinema.c
+++ cinema.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "cinema.h"
 
-char sala[ROWS][COLS];
+char salas[MAX_FILMES][ROWS][COLS];
 Bilhete bilhetes[MAX_TICKETS];
 int totalBilhetes = 0;
 int contadorBilhete = 1;
@@ -15,23 +15,25 @@ Filme filmes[MAX_FILMES] = {
     {"Duna: Parte 2", 2024}
 };
 
-void inicializarSala() {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            sala[i][j] = 'D';
+void inicializarSalas() {
+    for (int f = 0; f < MAX_FILMES; f++) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                salas[f][i][j] = 'D';
+            }
         }
     }
 }
 
 void inicializarSistema() {
-    inicializarSala();
+    inicializarSalas();
 }
 
-void exibirSala() {
-    printf("\nSala de Cinema:\n");
+void exibirSala(int filmeIndex) {
+    printf("\nSala do filme: %s\n", filmes[filmeIndex].nome);
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            printf("[%c] ", sala[i][j]);
+            printf("[%c] ", salas[filmeIndex][i][j]);
         }
         printf("\n");
     }
@@ -39,8 +41,6 @@ void exibirSala() {
 
 void listarFilmes() {
     printf("\nFilmes disponiveis (ordenados por ano de lancamento):\n");
-
-    
     for (int i = 0; i < MAX_FILMES - 1; i++) {
         for (int j = 0; j < MAX_FILMES - i - 1; j++) {
             if (filmes[j].anoLancamento > filmes[j + 1].anoLancamento) {
@@ -76,14 +76,15 @@ void reservarLugar() {
         return;
     }
 
-    exibirSala();
+    int filmeIndex = escolhaFilme - 1;
+    exibirSala(filmeIndex);
 
     printf("Escolha a linha (0-%d): ", ROWS - 1);
     scanf("%d", &linha);
     printf("Escolha a coluna (0-%d): ", COLS - 1);
     scanf("%d", &coluna);
 
-    if (sala[linha][coluna] == 'X') {
+    if (salas[filmeIndex][linha][coluna] == 'X') {
         printf("Assento ja reservado!\n");
         return;
     }
@@ -96,41 +97,20 @@ void reservarLugar() {
     printf("Tipo de ingresso (1 - Inteira / 2 - Meia): ");
     scanf("%d", &tipo);
 
-    sala[linha][coluna] = 'X';
+    salas[filmeIndex][linha][coluna] = 'X';
 
     Bilhete novoBilhete;
     novoBilhete.numero = contadorBilhete++;
     strcpy(novoBilhete.cliente, cliente);
-    strcpy(novoBilhete.filme, filmes[escolhaFilme - 1].nome);
+    strcpy(novoBilhete.filme, filmes[filmeIndex].nome);
     novoBilhete.linha = linha;
     novoBilhete.coluna = coluna;
+    novoBilhete.idFilme = filmeIndex;
     strcpy(novoBilhete.tipoIngresso, tipo == 1 ? "Inteira" : "Meia");
 
     bilhetes[totalBilhetes++] = novoBilhete;
 
     printf("Bilhete gerado com sucesso! Numero do bilhete: %d\n", novoBilhete.numero);
-}
-
-void imprimirBilhete(int numero) {
-    int inicio = 0, fim = totalBilhetes - 1, meio;
-
-    while (inicio <= fim) {
-        meio = (inicio + fim) / 2;
-        if (bilhetes[meio].numero == numero) {
-            printf("\n=== Bilhete Encontrado ===\n");
-            printf("Número: %d\n", bilhetes[meio].numero);
-            printf("Cliente: %s\n", bilhetes[meio].cliente);
-            printf("Filme: %s\n", bilhetes[meio].filme);
-            printf("Lugar: Linha %d, Coluna %d\n", bilhetes[meio].linha, bilhetes[meio].coluna);
-            printf("Tipo: %s\n", bilhetes[meio].tipoIngresso);
-            return;
-        } else if (bilhetes[meio].numero < numero) {
-            inicio = meio + 1;
-        } else {
-            fim = meio - 1;
-        }
-    }
-    printf("Bilhete nao encontrado.\n");
 }
 
 void ordenarBilhetesPorNumero(int crescente) {
@@ -158,6 +138,29 @@ void ordenarBilhetesPorTipo() {
     }
 }
 
+void imprimirBilhete(int numero) {
+    ordenarBilhetesPorNumero(1);
+    int inicio = 0, fim = totalBilhetes - 1, meio;
+
+    while (inicio <= fim) {
+        meio = (inicio + fim) / 2;
+        if (bilhetes[meio].numero == numero) {
+            printf("\n=== Bilhete Encontrado ===\n");
+            printf("Numero: %d\n", bilhetes[meio].numero);
+            printf("Cliente: %s\n", bilhetes[meio].cliente);
+            printf("Filme: %s\n", bilhetes[meio].filme);
+            printf("Lugar: Linha %d, Coluna %d\n", bilhetes[meio].linha, bilhetes[meio].coluna);
+            printf("Tipo: %s\n", bilhetes[meio].tipoIngresso);
+            return;
+        } else if (bilhetes[meio].numero < numero) {
+            inicio = meio + 1;
+        } else {
+            fim = meio - 1;
+        }
+    }
+    printf("Bilhete nao encontrado.\n");
+}
+
 void listarBilhetes() {
     int opcao;
     printf("\nEscolha a ordenacao:\n");
@@ -177,17 +180,20 @@ void listarBilhetes() {
             ordenarBilhetesPorTipo();
             break;
         default:
-            printf("Opcaoo invalida! Listando sem ordenacao.\n");
+            printf("Opcao invalida! Listando sem ordenacao.\n");
     }
 
     printf("\nLista de Bilhetes:\n");
     for (int i = 0; i < totalBilhetes; i++) {
-        printf("Numero: %d, Cliente: %s, Tipo: %s\n", bilhetes[i].numero, bilhetes[i].cliente, bilhetes[i].tipoIngresso);
+        printf("Numero: %d, Cliente: %s, Filme: %s, Tipo: %s\n",
+               bilhetes[i].numero, bilhetes[i].cliente,
+               bilhetes[i].filme, bilhetes[i].tipoIngresso);
     }
 }
 
 void menu() {
     int opcao;
+    int numero;
     do {
         printf("\n===== Sistema de Bilheteria =====\n");
         printf("1. Reservar lugar\n");
@@ -202,12 +208,19 @@ void menu() {
             case 1:
                 reservarLugar();
                 break;
-            case 2:
-                exibirSala();
+            case 2: {
+                listarFilmes();
+                int filmeEscolhido;
+                printf("Escolha o filme (1-%d): ", MAX_FILMES);
+                scanf("%d", &filmeEscolhido);
+                if (filmeEscolhido >= 1 && filmeEscolhido <= MAX_FILMES)
+                    exibirSala(filmeEscolhido - 1);
+                else
+                    printf("Filme invalido!\n");
                 break;
+            }
             case 3:
                 printf("Digite o numero do bilhete: ");
-                int numero;
                 scanf("%d", &numero);
                 imprimirBilhete(numero);
                 break;
